2560f4fa320b60a4ceebaf858cad3dc2501ad540
wingmatt <matt@wingwebhelp.com>
2020-09-29 13:11:13 -0700

    Add alt text to imgs

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <a href="/api/login">Login</a>
+      <a href="/api/logout">Logout</a>
       <ul className="card-list">
         <li>
           <Link href="/donate-with-checkout">
             <a className="card checkout-style-background">
               <h2 className="bottom">Donate with Checkout</h2>
-              <img src="/checkout-one-time-payments.svg" />
+              <img src="/checkout-one-time-payments.svg" alt="Donate with Checkout" />
             </a>
           </Link>
         </li>
@@ -19,7 +20,7 @@ const IndexPage: NextPage = () => {
           <Link href="/donate-with-elements">
             <a className="card elements-style-background">
               <h2 className="bottom">Donate with Elements</h2>
-              <img src="/elements-card-payment.svg" />
+              <img src="/elements-card-payment.svg" alt="Donate with Elements"/>
             </a>
           </Link>
         </li>
@@ -27,7 +28,7 @@ const IndexPage: NextPage = () => {
           <Link href="/use-shopping-cart">
             <a className="card cart-style-background">
               <h2 className="bottom">Use Shopping Cart</h2>
-              <img src="/use-shopping-cart.png" />
+              <img src="/use-shopping-cart.png" alt="Use Shopping Cart"/>
             </a>
           </Link>
         </li>
