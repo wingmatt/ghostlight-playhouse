12c9e41685e677b97990138b29e69d020c3949b3
wingmatt <matt@wingwebhelp.com>
2020-10-17 10:26:37 -0700

    Add fetchStripePrices

--- utils/get-stripe-price.js
+++ utils/get-stripe-price.js
@@ -7,13 +7,26 @@ export default async function fetchPriceFromStripe(priceId) {
   const stripePriceResponse =  await stripe.prices.retrieve(priceId)
   .then((response) => {
     let priceObject = {
-      id: response.id,
-      image: response.image,
+      sku: response.id,
       name: response.name,
       price: response.price,
       currency: response.currency,
       type: response.type
     }
-    return priceObject;
+    return stripePriceResponse;
+  });
+}
+
+export default async function fetchStripePrices() {
+  const stripePrices =  await stripe.prices.list()
+  .then((response) => {
+    let priceObject = {
+      sku: response.id,
+      name: response.name,
+      price: response.price,
+      currency: response.currency,
+      type: response.type
+    }
+    return stripePriceResponse;
   });
 }
