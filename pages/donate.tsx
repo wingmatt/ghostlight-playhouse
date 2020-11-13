8a580ba72017f40da5fa04abef51c097bc2276d9
wingmatt <matt@wingwebhelp.com>
2020-11-13 15:49:50 -0800

    Break up donate text w/ divider

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -15,9 +15,11 @@ const DonatePage: NextPage = () => {
         <p>It’s operated by the Randall Theatre Company, a non-profit theatre with over a decade of history in Downtown Medford.</p>
         <p>We provide live entertainment, whether it’s produced by the Randall, or other entertainers. Our job is to make it accessible to you.</p>
         <p>We are so grateful for all of the support from the Southern Oregon community, and we are honored to continue the tradition of the Randall Theatre Company at The Ghostlight Playhouse!</p>
+        <hr />
         <Elements stripe={getStripe()}>
           <ElementsForm />
         </Elements>
+        <p><b>Nonprofit Tax ID:</b> 35-2239724</p>
       </div>
     </Layout>
   )
