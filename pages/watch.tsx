d000710e2902c234ee8a6a733d5fd1c09c4a42c8
wingmatt <matt@wingwebhelp.com>
2020-11-12 16:09:19 -0800

    Add check for access:stream permissions

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -3,18 +3,26 @@ import Layout from '../components/Layout'
 import WithAuth from '../components/WithAuth';
 
 const WatchPage: NextPage = (props) => {
+  const isSubscribed = props.user.permissions.includes('access:stream')
 
-  return (
-    <Layout user={props.user}>
-      <h1>Livestream</h1>
-
-
-        <>
-          <div id='wowza_player'></div>
-          <script id='player_embed' src='https://player.cloud.wowza.com/hosted/rjrbb1t0/wowza.js' type='text/javascript'></script>
-        </>
-    </Layout>
-  )
+    if (isSubscribed) {
+      return(
+        <Layout user={props.user}>
+          <h1>Livestream</h1>
+          <>
+            <div id='wowza_player'></div>
+            <script id='player_embed' src='https://player.cloud.wowza.com/hosted/rjrbb1t0/wowza.js' type='text/javascript'></script>
+          </>
+        </Layout>
+      )
+    } else {
+      return(
+        <Layout user={props.user}>
+          <h1>Start Watching Live Local Talent</h1>
+          <button>Sign Up and Start Watching</button>
+        </Layout>
+      )
+    }
 }
 
 export default WithAuth(WatchPage)
