d04a1db49c7d2232fa64c39902829689d0e52b6d
wingmatt <matt@wingwebhelp.com>
2020-11-23 14:55:39 -0800

    Get Stripe Customer Portal link URL

--- utils/get-stripe-customer-portal.ts
+++ utils/get-stripe-customer-portal.ts
@@ -1,13 +1,13 @@
 import Stripe from "stripe";
 
 const getStripeCustomerPortal = async (stripeCustomerId) => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_LIVE, {
     apiVersion: "2020-03-02",
   });
 
   return await stripe.billingPortal.sessions
     .create({
-      customer: "cus_IRKnT1ZtkZVUN8",
+      customer: stripeCustomerId,
       return_url: "https://watch.ghostlightplayhouse.com/?refresh",
     })
     .then((response) => {
