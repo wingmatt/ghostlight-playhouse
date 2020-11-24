a066f661249de77dfa76f94a6ef675016f66f018
wingmatt <matt@wingwebhelp.com>
2020-11-23 16:40:28 -0800

    Move stripeCustomer ID set outside render return

--- pages/account.tsx
+++ pages/account.tsx
@@ -2,17 +2,21 @@ import CheckAuth from "../components/CheckAuth";
 import Layout from "../components/Layout";
 
 const Profile = (props) => {
-  const loggedIn = (props.user)
-let subscriptionStatus = "You are not subscribed."
-  if (props.user.permissions && props.user.permissions.includes('access:stream')) {
-    subscriptionStatus = "Your subscription is active!"
-}
+  const loggedIn = props.user;
+  const stripeCustomer = loggedIn ? props.user.stripe_customer : null;
+  let subscriptionStatus = "You are not subscribed.";
+  if (
+    props.user.permissions &&
+    props.user.permissions.includes("access:stream")
+  ) {
+    subscriptionStatus = "Your subscription is active!";
+  }
 
   return (
     <Layout title="Account Details | Ghostlight Playhouse" loggedIn={loggedIn}>
       <h1>Account Details</h1>
       <p>{subscriptionStatus}</p>
-      <a href={"/api/billing/"+props.user.stripe_customer}>Manage Billing</a>
+      <a href={"/api/billing/" + stripeCustomer}>Manage Billing</a>
     </Layout>
   );
 };
