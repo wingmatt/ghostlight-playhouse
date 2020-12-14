909fd0d8fcaab2e1e4c88f0017d14911d8467191
wingmatt <matt@wingwebhelp.com>
2020-12-14 15:45:21 -0800

    Start processing GQL response data

--- components/StreamButtons.tsx
+++ components/StreamButtons.tsx
@@ -20,13 +20,19 @@ export const ALL_PERFORMANCE_LINKS_QUERY = gql`
 
 export default function StreamButtons() {
   const { loading, error, data } = useQuery(ALL_PERFORMANCE_LINKS_QUERY);
+  
   if (error)
     return <div>Error loading buttons.</div>;
   if (loading)
     return <div>Loading</div>;
-  return (
-    <ul>
-      <li><a href=""></a></li>
-    </ul>
-  );
+  if(data){
+    const performances = data.products.edges;
+    console.log(performances);
+    return (
+      <ul>
+        <li></li>
+      </ul>
+    );
+  }
+    
 }
