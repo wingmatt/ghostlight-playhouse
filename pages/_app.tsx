67053c0f3e937c003a0c80f5b1787266a7e04e0f
wingmatt <matt@wingwebhelp.com>
2020-12-14 08:50:01 -0800

    Add more Apollo util functions and add it to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
 
 import '../styles.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
 
-export default MyApp
+  return (
+    <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
