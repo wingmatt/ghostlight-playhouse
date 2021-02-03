07fbd36e2bc8e5a7b532f528a3f46551a7c6c878
wingmatt <matt@wingwebhelp.com>
2021-02-03 09:00:02 -0800

    Add date-specific formatting and loop options

--- components/UpcomingEvents.tsx
+++ components/UpcomingEvents.tsx
@@ -1,17 +1,14 @@
 import { gql, useQuery } from "@apollo/client";
 
-export const ALL_PERFORMANCE_LINKS_QUERY = gql`
-  query PerformanceLinks {
-    products {
+export const UPCOMING_EVENTS_QUERY = gql`
+  query UpcomingEvents {
+    events {
       edges {
         node {
-          performanceDetails {
-            performanceLinks {
-              linkText
-              linkUrl
-            }
-            performanceDate
-          }
+          id
+          startDate
+          title
+          uri
         }
       }
     }
@@ -19,28 +16,26 @@ export const ALL_PERFORMANCE_LINKS_QUERY = gql`
 `;
 
 export default function StreamLinks() {
-  const { loading, error, data } = useQuery(ALL_PERFORMANCE_LINKS_QUERY);
-  const today = new Date().toLocaleDateString("en-US");
+  const { loading, error, data } = useQuery(UPCOMING_EVENTS_QUERY);
   if (error) console.log(error);
   if (loading)
     return <img src="/loading.svg" alt="Loading" width="25px" height="25px" />;
   if (data) {
-    const performances = data.products.edges;
-    let todayPerformances = [];
-    performances.forEach((performance) => {
-      let performanceDate = performance.node.performanceDetails;
-      let performanceLinks =
-        performance.node.performanceDetails.performanceLinks;
-      if (performanceLinks) {
-        todayPerformances = todayPerformances.concat(performanceLinks);
-      }
-    });
+    const events = data.products.edges;
+    const dateOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'PST',
+      timeZoneName: 'short'
+    }
     return (
-      <ul className="stream_links">
-        {todayPerformances.map((link, index) => (
-          <li key={`${index}_${link.linkUrl}_${link.linkText}`} className="stream_link">
-            <a href={link.linkUrl} target="blank">
-              {link.linkText}
+      <ul className="upcoming_events">
+        {events.map((event, index) => (
+          <li key={`${index}_${event.linkUrl}_${event.linkText}`} className="upcoming_event">
+            <a href={"https://ghostlightplayhouse.com" + event.linkUrl} target="blank">
+              {event.stateDate.toLocaleDateString('en-US', dateOptions)}: {event.title}
             </a>
           </li>
         ))}
