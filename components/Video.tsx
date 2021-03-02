d09d324ae3bd42020246013bf0d07ae628dbcdfe
wingmatt <matt@wingwebhelp.com>
2021-03-02 08:25:59 -0800

    Shrink videos to allow 2-col layout on lg screens

--- components/Video.tsx
+++ components/Video.tsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player'
 const Video = (props) => {
  const videoUrl = props.videoUrl;
  return (
-   <ReactPlayer url={videoUrl} controls="true" />
+   <ReactPlayer url={videoUrl} controls width="500px" height="282px" />
  )
 }
 
