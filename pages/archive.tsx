e32232553c3f0ef48bec2d244401c7016a14ea35
wingmatt <matt@wingwebhelp.com>
2021-03-02 08:26:30 -0800

    Add list of links to archive

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -5,9 +5,33 @@ import Video from "../components/Video";
 const Archive = (props) => {
   const loggedIn = props.user;
   const videos = {
-    Rickrolls: [
-      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "Randall Productions": [
+      "https://www.youtube.com/watch?v=Rcht1SVosxM",
+      "https://www.youtube.com/watch?v=sfp-PxAObc0",
+      "https://www.youtube.com/watch?v=qh0awAPoYWU",
+      "https://www.youtube.com/watch?v=6iP8NMDER1s",
+      "https://www.youtube.com/watch?v=Cb6egEegn3o",
+      "https://www.youtube.com/watch?v=hF27liWubQ4",
+      "https://www.youtube.com/watch?v=ejlFE8qx2X0",
+      "https://www.youtube.com/watch?v=j08gcJTs60s",
+      "https://www.youtube.com/watch?v=JfbjJZczRnc",
+      "https://www.youtube.com/watch?v=88YC4qXDFA4",
+      "https://www.youtube.com/watch?v=htWcf69OBts",
+      "https://www.youtube.com/watch?v=_pI9C0LsNRY",
+      "https://www.youtube.com/watch?v=l17alob1YRk",
+      "https://www.youtube.com/watch?v=T-kXBZH_aTg",
+      "https://www.youtube.com/watch?v=ISKOC9oAKpw",
+      "https://www.youtube.com/watch?v=fhHOGyffaTE",
+      "https://www.youtube.com/watch?v=-NJMDiWC1Oc"
+    ],
+    Comedy: [
+      "https://www.youtube.com/watch?v=D0-0e8OT38E",
+      "https://www.youtube.com/watch?v=AAeuLKuUqIo",
+      "https://www.youtube.com/watch?v=L8OVixkh7vE",
+      "https://www.youtube.com/watch?v=S_etDaFcuSg"
+    ],
+    Music: [
+      "https://www.youtube.com/watch?v=hG2fILyMlGg"
     ]
   };
 
